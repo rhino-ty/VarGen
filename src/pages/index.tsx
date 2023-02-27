f9181c212c47456cc1bf7acab7a57e109d4eb44d
Rhino-ty <okko8522@gmail.com>
2023-02-27 15:53:51 +0900

    feat: 타입 설정 및 리액트 쿼리 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
+import { getChatResponse } from "./api/chatai";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const inter = Inter({ subsets: ["latin"] });
+interface ChatResponseProps {
+  prompt: string;
+}
 
 export default function Home() {
+  const queryClient = new QueryClient();
+
+  const ChatResponse = ({ prompt }: ChatResponseProps) => {
+    const { data } = useQuery(["chatResponse", prompt], () => getChatResponse(prompt));
+    console.log(data);
+    return <div>{data?.resText}</div>;
+  };
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>Vargen</title>
         <meta name="description" content="변수 생성 앱" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+      {/* <ChatResponse prompt={`Create 10 login related variable names with React`} /> */}
+    </QueryClientProvider>
   );
 }
