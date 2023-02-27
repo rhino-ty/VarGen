eb8a6064228a916d6fedfa6638cb86caad4e81bb
Rhino-ty <okko8522@gmail.com>
2023-02-27 14:55:51 +0900

    feat: 타입 설정 및 변수명 수정

--- src/pages/api/chatai.ts
+++ src/pages/api/chatai.ts
@@ -1,29 +1,31 @@
 import axios from "axios";
 
-export const chatAi = async (data: string) => {
-  try {
-    // axios를 이용해서 chatgpt와 통신
-    const pos = await axios.post(
-      "https://api.openai.com/v1/completions",
-      {
-        model: "text-davinci-003",
-        prompt: `${data}`,
-        temperature: 0.9,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0.6,
-        stop: [" Human:", " AI:"],
+interface ChatResponseData {
+  resText: string;
+  resId: string;
+}
+
+export const getChatResponse = async (prompt: string): Promise<ChatResponseData> => {
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.9,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPEN_API}`,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + String(process.env.NEXT_PUBLIC_OPEN_API),
-        },
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    alert("오류가 발생하였습니다.");
-  }
+    }
+  );
+  const resText = response.data.choices[0].text;
+  const resId = response.data.id;
+  return { resText, resId };
 };
