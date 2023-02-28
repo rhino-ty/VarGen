85250f8d2edb99d22033456111e6d9e1fd1f7f15
Rhino-ty <okko8522@gmail.com>
2023-02-28 14:55:51 +0900

    feat: try, catch 문 넣어 에러처리 추가

--- src/pages/api/chatai.ts
+++ src/pages/api/chatai.ts
@@ -6,26 +6,31 @@ interface ChatResponseData {
 }
 
 export const getChatResponse = async (prompt: string): Promise<ChatResponseData> => {
-  const response = await axios.post(
-    "https://api.openai.com/v1/completions",
-    {
-      model: "text-davinci-003",
-      prompt,
-      temperature: 0.9,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPEN_API}`,
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.9,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+        stop: [" Human:", " AI:"],
       },
-    }
-  );
-  const resText = response.data.choices[0].text;
-  const resId = response.data.id;
-  return { resText, resId };
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPEN_API}`,
+        },
+      }
+    );
+    const resText = response.data.choices[0].text;
+    const resId = response.data.id;
+    return { resText, resId };
+  } catch (error) {
+    console.error(`Error in getChatResponse: ${error}`);
+    throw new Error("Unable to generate chat response");
+  }
 };
