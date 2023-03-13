757628043c687ea36aea194d9de58f87653b79c1
Rhino-ty <okko8522@gmail.com>
2023-03-13 11:16:02 +0900

    feat: API 3.5 출시로 모델 변경
    url 변경 및 POST할 때의 body 변경

--- src/pages/api/chatai.ts
+++ src/pages/api/chatai.ts
@@ -7,17 +7,37 @@ interface ChatResponseData {
 
 export const getChatResponse = async (prompt: string): Promise<ChatResponseData> => {
   try {
+    // 다빈치 사용
+    // const response = await axios.post(
+    //   "https://api.openai.com/v1/completions",
+    //   {
+    //     model: "text-davinci-003",
+    //     prompt,
+    //     temperature: 0.9,
+    //     max_tokens: 100,
+    //     top_p: 1,
+    //     frequency_penalty: 0,
+    //     presence_penalty: 0.6,
+    //     stop: [" Human:", " AI:"],
+    //   },
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPEN_API}`,
+    //     },
+    //   }
+    // );
+    // const resText = response.data.choices[0].text;
+    // const resId = response.data.id;
+
+    // gpt 3.5 터보 사용
     const response = await axios.post(
-      "https://api.openai.com/v1/completions",
+      "https://api.openai.com/v1/chat/completions",
       {
-        model: "text-davinci-003",
-        prompt,
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
         temperature: 0.9,
         max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0.6,
-        stop: [" Human:", " AI:"],
       },
       {
         headers: {
@@ -26,8 +46,9 @@ export const getChatResponse = async (prompt: string): Promise<ChatResponseData>
         },
       }
     );
-    const resText = response.data.choices[0].text;
+    const resText = response.data.choices[0].message.content;
     const resId = response.data.id;
+
     return { resText, resId };
   } catch (error) {
     console.error(`Error in getChatResponse: ${error}`);
