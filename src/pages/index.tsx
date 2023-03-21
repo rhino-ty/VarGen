17077450d27e6b59676d6f7e1be3f2cb88a96414
Rhino-ty <okko8522@gmail.com>
2023-03-21 13:38:02 +0900

    feat: .bg-primary 및 Header 기본 재료 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import PromptInput from "@/components/PromptInput";
 import Head from "next/head";
+import DarkModeToggleButton from "../components/DarkModeToggleButton";
 
 export default function Home() {
   return (
@@ -10,6 +11,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className="text-gray-600 body-font md:sticky md:top-0 md:z-50 bg-primary">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <DarkModeToggleButton />
+        </div>
+      </header>
+
       <PromptInput />
     </>
   );
