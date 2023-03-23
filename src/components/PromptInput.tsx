53486765396f720a2ab2f181682e347765333de1
Rhino-ty <okko8522@gmail.com>
2023-03-23 13:28:32 +0900

    feat: input 주석처리 하고 state 3개 추가
    countVariable, subject, namingConvention

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ChatResponse } from "./ChatResponse";
 
 const queryClient = new QueryClient();
 
-export default function PromptInput() {
-  const [inputValue, setInputValue] = useState("");
-  const [promptValue, setPromptValue] = useState("");
+type CountVariable = 5 | 10 | 20;
+type NamingConvention = "camelCase" | "PascalCase" | "snake_case" | "";
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+export default function PromptInput() {
+  // const [inputValue, setInputValue] = useState("");
+  const [countVariable, setCountVariable] = useState<CountVariable>(5);
+  const [subject, setSubject] = useState<string>("");
+  const [namingConvention, setNamingConvention] = useState<NamingConvention>("");
+  const [promptValue, setPromptValue] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     queryClient.invalidateQueries("chatResponse");
-    if (inputValue !== "") {
-      setPromptValue(inputValue);
-    }
+    setPromptValue(
+      `Create ${countVariable} ${subject} related variable names with ${namingConvention}`
+    );
+    //   if (inputValue !== "") {
+    //     setPromptValue(inputValue);
+    //   }
   };
 
   return (
     <QueryClientProvider client={queryClient}>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        {/* <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} /> */}
+
         <button type="submit">Submit</button>
       </form>
       {/* <ChatResponse prompt={`Create 10 게시판 related variable names with camelCase`} /> */}
